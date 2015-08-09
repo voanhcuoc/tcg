16e5c808dba11d670e0c36ead40ebfbadfb217dd
Chế Tiệp Chân Khoa <lichkingharass@gmail.com>
2015-08-09 18:14:49 +0700

    Monster cards' base class

---
+++ card/monster.py
@@ -0,0 +1,32 @@
+# Monster card definition
+# Normal monster, Effect monster, Fusion monster, Ritual monster, Synchro monter, XYZ monster
+
+from .card import Card
+
+class Monster(Card):
+    'Root class for monster card'
+    def __init__(self, name, types, attr, descr, effs, location,
+                 attack, defend, star):
+        Card.__init__(self, name, types, attr, descr, effs, location)
+        self.attack = attack
+        self.defend = defend
+        self.star = int(star)
+
+    def __repr__(self):
+        return Card.__repr__(self) + '\nStar: {} Attack: {} Defend: {}'.format(
+            self.star, self.attack, self.defend)
+
+    def discard(self):
+        self.location.remove(self)
+
+    def attack(self, other):
+        battle(self, other)
+
+    def normalSummon(self):
+        self.location.summon(self)
+
+    def set(self):
+        self.location.set(self)
+
+    def flip(self):
+        self.location.flip(self)
